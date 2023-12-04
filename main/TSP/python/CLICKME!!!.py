7d1282340996f551e67d7f28866a7b3df1c14b2f
KainuMan <148784729+KainuMan@users.noreply.github.com>
2023-12-04 19:58:49 +0700

    Update CLICKME!!!.py
    Time teller implemented?

--- main/TSP/python/CLICKME!!!.py
+++ main/TSP/python/CLICKME!!!.py
@@ -1,9 +1,16 @@
 from TSP import *
 from RNG import *
+import time
 
 n = int(input("Number of cities: "))
 seed = seed_input()
+
+start_time = time.time() #Start the execution
 if seed: d = RNG(seed).distance(n)
 else: d = RNG().distance(n)
 tsp = TSP(n, d)
 print("Distance of the cities:",d,"\nShortest way is:",tsp[1],"with the distance of:",tsp[0])
+end_time = time.time() #Ends the execution
+
+counted_time = end_time - start_time
+print(f"Calculation time: {counted_time} seconds.")
