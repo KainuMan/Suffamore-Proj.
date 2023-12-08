3b6dd47209296f9cd35c0b3a2b2d49ab57c5f371
Mikey <149621439+MikeStorm03@users.noreply.github.com>
2023-12-08 23:21:08 +0700

    i just make it run faster but not sure why i'm still update this broken thing

--- main/TSP/python/TSP.py
+++ main/TSP/python/TSP.py
@@ -5,18 +5,15 @@ def TSP(n, distance = None):
     shortest = None
     mincost = None
     if distance is None: distance = distance_input(n)
-    for i in permutations(range(n), n):  #Permutation the number so we can get all the path
-        if i[0] != 0: continue  #Since we start at 0, so if the starting point is not 0, we skip
-        s = 0
+    for i in permutations(range(1,n), n-1):  #Permutation the number so we can get all the path
         for j in i:
-            if j == 0:
-                k = j
-                continue
-            if k > j: s += distance[(j,k)] #Just sum up the distance, you might know the rest of it
-            else: s += distance[(k,j)]
+            if j == i[0]: s = distance[i[0]][0]
+            elif k > j: s += distance[k][j] #Just sum up the distance, you might know the rest of it
+            else: s += distance[j][k]
+            if mincost:
+                if s > mincost: break
             k = j
-        if shortest is None: shortest = i
-        if mincost is None: mincost = s
+        if not mincost: mincost = s
         if mincost > s:
             shortest = i
             mincost = s
