b28ab5ecbcf8e75d9383c8e98b51d8989cd28954
Mikey <149621439+MikeStorm03@users.noreply.github.com>
2023-12-17 08:40:47 +0700

    current nide and expanded node in the results

--- main/TSP/python/new code/TSP.py
+++ main/TSP/python/new code/TSP.py
@@ -17,17 +17,18 @@ class TSP:
         map.append(a)
         return map
     
-    def Mystery_Heuristic(self):
+    def min_out_Heuristic(self):
         s = 0
         m= self.world_tour_gen()
-        x = 0
+        x = 0 #current city
         while m[1].count(True)<self.n:
-            d=[[],[]]
+            d=[[],[]] #open node
             for i in range(1,self.n):
                 if i == x or m[1][i] == True: continue
                 if i > x: d[1].append(self.distance[i][x])
                 else: d[1].append(self.distance[x][i])
                 d[0].append(i)
+            print("Current node:",x,"\tExpanded node:",d[0])
             s += min(d[1])
             x = d[0][d[1].index(min(d[1]))]
             m[0].append(x)
